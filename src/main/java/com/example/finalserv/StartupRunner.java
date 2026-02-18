7a0e7c23421d0d6562a011109f2749dc718843bb
Harikaran R <131746803+Harikaran1212@users.noreply.github.com>
2026-02-18 19:07:20 +0530

    Clean up comments in StartupRunner.java
    Removed comments for steps and questions in the run method.

--- src/main/java/com/example/finalserv/StartupRunner.java
+++ src/main/java/com/example/finalserv/StartupRunner.java
@@ -14,7 +14,6 @@ public class StartupRunner implements CommandLineRunner {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        // STEP 1 — Generate Webhook
         String generateUrl = "https://bfhldevapigw.healthrx.co.in/hiring/generateWebhook/JAVA";
 
         WebhookRequest request = new WebhookRequest(
@@ -34,7 +33,6 @@ public class StartupRunner implements CommandLineRunner {
 
         String regNo = request.getRegNo();
 
-// extract last 2 digits
         int lastTwoDigits = Integer.parseInt(regNo.substring(regNo.length()-2));
 
         System.out.println("Last 2 digits: " + lastTwoDigits);
@@ -45,18 +43,15 @@ public class StartupRunner implements CommandLineRunner {
 
             System.out.println("EVEN → Executing Question 2");
 
-            // QUESTION 2
             sqlQuery = "SELECT d.DEPARTMENT_NAME, AVG(TIMESTAMPDIFF(YEAR, e.DOB, CURDATE())) AS AVERAGE_AGE, SUBSTRING_INDEX(GROUP_CONCAT(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) ORDER BY e.EMP_ID SEPARATOR ', '), ', ', 10) AS EMPLOYEE_LIST FROM DEPARTMENT d JOIN EMPLOYEE e ON d.DEPARTMENT_ID = e.DEPARTMENT JOIN PAYMENTS p ON e.EMP_ID = p.EMP_ID WHERE p.AMOUNT > 70000 GROUP BY d.DEPARTMENT_ID, d.DEPARTMENT_NAME ORDER BY d.DEPARTMENT_ID DESC;";
 
         } else {
 
             System.out.println("ODD → Executing Question 1");
 
-            // QUESTION 1
             sqlQuery = "WITH salary_data AS ( SELECT d.DEPARTMENT_ID, d.DEPARTMENT_NAME, e.EMP_ID, CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS EMPLOYEE_NAME, TIMESTAMPDIFF(YEAR, e.DOB, CURDATE()) AS AGE, SUM(p.AMOUNT) AS SALARY, ROW_NUMBER() OVER ( PARTITION BY d.DEPARTMENT_ID ORDER BY SUM(p.AMOUNT) DESC ) AS rn FROM DEPARTMENT d JOIN EMPLOYEE e ON d.DEPARTMENT_ID = e.DEPARTMENT JOIN PAYMENTS p ON e.EMP_ID = p.EMP_ID WHERE DAY(p.PAYMENT_TIME) <> 1 GROUP BY d.DEPARTMENT_ID, d.DEPARTMENT_NAME, e.EMP_ID, e.FIRST_NAME, e.LAST_NAME, e.DOB ) SELECT DEPARTMENT_NAME, SALARY, EMPLOYEE_NAME, AGE FROM salary_data WHERE rn = 1;";
         }
 
-        // STEP 3 — Submit Answer
         String submitUrl = "https://bfhldevapigw.healthrx.co.in/hiring/testWebhook/JAVA";
 
         HttpHeaders headers = new HttpHeaders();
